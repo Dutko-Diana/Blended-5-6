edc8650a983bacfa5015c50afdbed3dbe917e8b5
Diana Dutko <dutkodiana2007@gmail.com>
2024-10-26 17:03:55 +0300

    task 6-7

--- js/task-06.js
+++ js/task-06.js
@@ -1,2 +1,13 @@
 // Напишіть функцію makeCounter, яка повертає іншу функцію,
 //     яка рахує та виводить у консоль кількість своїх викликів
+
+const makeCounter = () => {
+    let start = 0;
+    return () => start += 1;
+}
+
+const count = makeCounter();
+console.log(count());
+console.log(count());
+console.log(count());
+console.log(count());
